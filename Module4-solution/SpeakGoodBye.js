4e583d9d2497695def10a9768cb840706b6cd44c
Jonafa1 <yona.fa@gmail.com>
2021-08-30 12:26:22 +0300

    mod4-6

--- Module4-solution/SpeakGoodBye.js
+++ Module4-solution/SpeakGoodBye.js
@@ -1,4 +1,4 @@
-(function () {
+(function (window) {
 // NOTE! The steps in this file are basically identical to the ones you
 // performed in the SpeakHello.js file.
 
@@ -12,18 +12,20 @@
 // var byeSpeaker =
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
+var byeSpeaker = {};
 var speakWord = "Good Bye";
-var byeSpeaker = {}
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-byeSpeaker.speak =function () {
+byeSpeaker.speak = function () {
   console.log(speakWord + " " + name);
-  hello = bye
 }
+
+window.byeSpeaker = byeSpeaker;
+
 }
-)
+)(window)
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
--- Module4-solution/SpeakHello.js
+++ Module4-solution/SpeakHello.js
@@ -1,4 +1,4 @@
-(function () {
+(function (window) {
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
 
@@ -9,17 +9,18 @@
 // var helloSpeaker =
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
-var speakWord = "Hello";
 var helloSpeaker = {};
+var speakWord = "Hello";
+
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-helloSpeaker.speak(names) = function () {
+helloSpeaker.speak = function () {
   console.log(speakWord + " " + name);
-  names = helloSpeaker;
 }
+window.helloSpeaker = helloSpeaker;
 }
-)
+)(window)
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
 // 'helloSpeaker' on the global scope as well.
 // See Lecture 52, part 2
